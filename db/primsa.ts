199279a465b8192ad1103adfd7b293675a482aca
mrnia204 <204codetribe@gmail.com>
2025-11-03 15:47:28 +1000

    Product list updated with prisma.
    On branch product/updated
    Changes to be committed:
    	new file:   app/(root)/product/[slug]/page.tsx
    	new file:   components/shared/products/product-images.tsx
    	new file:   components/ui/badge.tsx
    	modified:   db/primsa.ts
    	modified:   lib/actions/product.actions.ts
    	modified:   prisma/schema.prisma

--- db/primsa.ts
+++ db/primsa.ts
@@ -5,28 +5,26 @@ import ws from 'ws';
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
+
+// Load your connection string from .env
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-//const pool = new Pool({ connectionString });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon({ connectionString });
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient({ adapter }).$extends({
-  result: {
-    product: {
-      price: {
-        compute(product) {
-          return product.price.toString();
-        },
-      },
-      rating: {
-        compute(product) {
-          return product.rating.toString();
-        },
+// use a global instance to prevent multiple clients during hot reloads
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// Initialize Prisma Client with adapter + result transformer
+export const prisma = globalForPrisma.prisma ||
+  new PrismaClient({ adapter}).$extends({
+    result: {
+      product: {
+        price: { compute: (p) => p.price?.toString()},
+        rating: { compute: (p) => p.rating?.toString()},
       },
     },
-  },
-});
+  });
+
+  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
